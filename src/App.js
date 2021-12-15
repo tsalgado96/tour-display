d2e535b6052c18c17827c805e2703a3fd3538d62
TJ Salgado <tsalgado96@gmail.com>
2021-12-16 06:30:05 +0700

    Revert back to working app

--- src/App.js
+++ src/App.js
@@ -36,7 +36,6 @@ const App = () => {
     <div className='container'>
       <h1>{isLoading ? 'Loading...' : isError ? 'Error...' : 'Our Tours'}</h1>
       <span className='underline'></span>
-      test for netlify
       <div className='tours-container'>
         {data.length > 0 ? (
           data.map(elem => {
